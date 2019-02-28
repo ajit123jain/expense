ce1ccb411be119d257f7b4de1701cd1b7a04a57e
ajit123jain <ajitkumarjain123@gmail.com>
2019-02-28 23:58:16 +0530

    dd

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,8 @@
 class WelcomeController < ApplicationController
   def index
     @comanies = 5
-    @followerscount = JSON.parse(client.user('ajit123jain').to_json)["followers_count"]
-    client = LinkedIn::Client.new(ENV['LINKEDIN_CLIENT_ID'], ENV['LINKEDIN_CLIENT_SECRET'])
+    # @followerscount = JSON.parse(client.user('ajit123jain').to_json)["followers_count"]
+    # client = LinkedIn::Client.new(ENV['LINKEDIN_CLIENT_ID'], ENV['LINKEDIN_CLIENT_SECRET'])
   end
 end
 
